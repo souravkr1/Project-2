e56c0bd41b3c4b3d5175187b0aee512282c014e0
souravkr1 <souravmech@gmail.com>
2015-01-25 16:50:22 +0530

    First commit

---
+++ plot2.R
@@ -0,0 +1,13 @@
+#  Read Data 
+NEI <- readRDS("./summarySCC_PM25.rds")
+
+#  Create Data
+BC <- subset(NEI, fips == "24510")
+pd <- aggregate(BC[c("Emissions")], list(year = BC$year), sum)
+
+#  Create Plot
+png('plot2.png', width=480, height=480)
+plot(pd$year, pd$Emissions, type = "l", 
+     main = "Total Emissions from PM2.5 in Baltimore City",
+     xlab = "Year", ylab = "Emissions")
+dev.off()
